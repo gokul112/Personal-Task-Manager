294fbea2838bce21c96a45850969fa41425a1833
gokul raj <gokul.raj@gigamon.com>
2020-07-19 19:54:30 +0530

    Updates

---
+++ precache-manifest.f076f7a79d2adc8cbdf67fbc6a7165b6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8e64b8ab68362b38410ef5de8dc582f8",
+    "url": "/Personal-Task-Manager/index.html"
+  },
+  {
+    "revision": "927f2cbbba880c75c665",
+    "url": "/Personal-Task-Manager/static/css/main.6dd284f3.chunk.css"
+  },
+  {
+    "revision": "81cc9394cf7d474523f3",
+    "url": "/Personal-Task-Manager/static/js/2.13e94d7b.chunk.js"
+  },
+  {
+    "revision": "a1a636632721a402612df3e043dd630d",
+    "url": "/Personal-Task-Manager/static/js/2.13e94d7b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "927f2cbbba880c75c665",
+    "url": "/Personal-Task-Manager/static/js/main.2b257287.chunk.js"
+  },
+  {
+    "revision": "0be0b3381b8b2e8261a7",
+    "url": "/Personal-Task-Manager/static/js/runtime-main.a128405e.js"
+  }
+]);
